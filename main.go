de47faae0575eff7ed3aecf39c604d35ff46baba
Matt <matt@buildyourweb.app>
2026-03-19 10:35:26 -0700

    feat: bump droplet to 32gb ram 8cpu to match deployed droplet

--- main.go
+++ main.go
@@ -50,7 +50,7 @@ func main() {
 			Image:    pulumi.String("ubuntu-24-04-x64"),
 			Name:     pulumi.String("nomad-server-01"),
 			Region:   region,
-			Size:     digitalocean.DropletSlugDropletS1VCPU512MB10GB,
+			Size:     digitalocean.DropletSlug_DropletS8VCPU32GB_AMD,
 			Ipv6:     pulumi.Bool(true),
 			Tags:     pulumi.StringArray{pulumi.String("nomad")},
 			SshKeys:  pulumi.ToStringArray(sshFingerprints),
